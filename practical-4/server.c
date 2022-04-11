e511246f2f72b27680aaeb9b2bb3bad3b477691c
Tlholo Koma <tlholokoma@gmail.com>
2022-04-11 23:24:17 +0200

    :sparkles: socket creation added

--- practical-4/server.c
+++ practical-4/server.c
@@ -13,8 +13,61 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+#define PORT 5555
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int server_fd, new_socket;
+    long valread;
+    struct sockaddr_in address;
+    int addrlen=sizeof(address);
+
+    //Creating socket
+    if ((server_fd=socket(AF_INET, SOCK_STREAM, 0))==0) {
+        perror("In socket");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family=AF_INET;
+    address.sin_addr.s_addr=INADDR_ANY;
+    address.sin_port=htons(PORT);
+
+    memset(address.sin_zero, '\0', sizeof(address.sin_zero));
+
+    // Connect to remote server
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
+        perror("In bind");
+        exit(EXIT_FAILURE);
+    }
+
+    // Adding listener
+    if (listen(server_fd, 10)<0) {
+        perror("In listen");
+        exit(EXIT_FAILURE);
+    }
+
+
+    int i = 0;
+    char header[3000];
+
+    while (1) {
+        bool equal = false;
+        printf("Waiting for new connection...\n\n");
+
+        // Waiting for upcoming client
+        if ((new_socket=accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
+            perror("In accept");
+            exit(EXIT_FAILURE);
+        }
+
+
+        char buffer[1000]={0};
+        valread=read(new_socket, buffer, 1000);
+        printf("%s\n", buffer);
+
+        
+        close(new_socket);
+    }
+    
     return 0;
 }
