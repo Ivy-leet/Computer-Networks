9baa72720797d0a9a2c02a154ab55ef861242e2e
Tlholo Koma <tlholokoma@gmail.com>
2022-04-11 23:20:18 +0200

    :tada:

---
+++ practical-4/server.c
@@ -0,0 +1,20 @@
+/*
+* 20575085, Koma T(Tlholo)
+* 20456078, Dzimati BM(Malcolm)
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
