0c7a1a8676822369cb2d3e5707d2bf0ba96395a0
malcolmdzimati <malcolmdzimati@gmail.com>
2022-04-26 03:50:44 +0200

    almost donw

--- practical-4/server.c
+++ practical-4/server.c
@@ -541,8 +541,7 @@ bool delete(char* site, char buffer[MAX_LENGTH]) {
             // i++;
             //cntl++;
         }else{
-            strcat(site, str);
-            strcat(site, nl);
+            printPerson(site, str);
         }
         i++;
     }
@@ -592,7 +591,7 @@ void getFormInsert(char* site){
     char* date=getCurrentDate();
     char* lastModified=getLastModifiedDate();
     char* server="Server: Maverick\n";
-    char* title="\n\n<?xml>\n<!DOCTYPE hmtl>\n<html>\n<head><link rel='icon' href='data:,'><style>\n</style>\n</head>\n<body>\n<h2>Insert Contact</h2>\n<form method='post'  action='i'><label for='fsname'>Surname Name:</label><br>\n<input type='text' id='fsname' name='fsname'><br>\n<label for='fname'>Name:</label><br>\n<input type='text' id='fname' name='fname'><br>\n<label for='fnumber'>Phone number:</label>\n<br><input type='text' id='fnumber' name='fnumber'><br><br>\n<input type='file' id='myfile' name='filename'><br><br><input type='submit' value='Submit'>\n</form><br>\n<form method='get' action='w'><input type='submit' value='Back To Menue'></form></body>\n</html>";
+    char* title="\n\n<?xml>\n<!DOCTYPE hmtl>\n<html>\n<head><link rel='icon' href='data:,'><style>\n</style>\n</head>\n<body>\n<h2>Insert Contact</h2>\n<form method='post' action='i'><label for='fsname'>Surname Name:</label><br>\n<input type='text' id='fsname' name='fsname'><br>\n<label for='fname'>Name:</label><br>\n<input type='text' id='fname' name='fname'><br>\n<label for='fnumber'>Phone number:</label>\n<br><input type='text' id='fnumber' name='fnumber'><br><input type='submit' value='Submit'>\n</form><br>\n<form method='get' action='w'><input type='submit' value='Back To Menue'></form></body>\n</html>";
 
     char* contentLength=getContentLength(title);
 
