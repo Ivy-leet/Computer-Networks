f74853fab378ceb5fba93b42813e27cd0b7c17c0
Tlholo Koma <tlholokoma@gmail.com>
2022-05-09 04:16:43 +0200

    :sparkles: connection to SMTP server established

--- practical-6/server.c
+++ practical-6/server.c
@@ -12,62 +12,58 @@
 #include <time.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 
-#define PORT 5555
+#define PORT 25
 #define MAX_LENGTH 256
 #define database ("database.txt")
 
+const char* getIPAddr(const char*);
+int connectToServer(const char*);
 
 int main(int argc, char const *argv[])
 {
-    
-    
-    int server_fd, new_socket;
-    long valread;
-    struct sockaddr_in address;
-    int addrlen=sizeof(address);
+    int connectfd=connectToServer("smtp.gmail.com");
 
-    //Creating socket
-    if ((server_fd=socket(AF_INET, SOCK_STREAM, 0))==0) {
-        perror("In socket");
-        exit(EXIT_FAILURE);
+    if (connectfd!=0) {
+        printf("connected to server!\n");
     }
-
-    address.sin_family=AF_INET;
-    address.sin_addr.s_addr=INADDR_ANY;
-    address.sin_port=htons(PORT);
-
-    memset(address.sin_zero, '\0', sizeof(address.sin_zero));
-
-    // Connect to remote server
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
-        perror("In bind");
-        exit(EXIT_FAILURE);
-    }
-
-    // Adding listener
-    if (listen(server_fd, 10)<0) {
-        perror("In listen");
-        exit(EXIT_FAILURE);
-    }
-
-    while (1) {
-        printf("Waiting for new connection...\n\n");
-
-        // Waiting for upcoming client
-        if ((new_socket=accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
-            perror("In accept");
-            exit(EXIT_FAILURE);
-        }
-
-
-        char buffer[1000]={0};
-        valread=read(new_socket, buffer, 1000);
-        write(new_socket, buffer, strlen(buffer));
-
-        close(new_socket);
-    }
-    
     
     return 0;
 }
+
+const char* getIPAddr(const char* target_domain) {
+    const char* target_ip;
+    struct in_addr *host_address;
+    struct hostent *raw_list=gethostbyname(target_domain);
+    if (raw_list==NULL) {
+        printf("Error: no such host name");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i=0; raw_list->h_addr_list[i]!=0; i++) {
+        host_address=raw_list->h_addr_list[i];
+        target_ip=inet_ntoa(*host_address);
+    }
+
+    return target_ip;
+}
+
+
+int connectToServer(const char* server_add) {
+    int socket_fd=socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+    
+    struct sockaddr_in address;
+    bzero(&address, sizeof(address));
+    address.sin_family=AF_INET;
+    
+    address.sin_port=htons(PORT);
+    
+    if (inet_pton(AF_INET, getIPAddr(server_add), &address.sin_addr)==1) {
+        connect(socket_fd, (struct sockaddr*)&address, sizeof(address));
+    }
+    return socket_fd;
+}
+
+
