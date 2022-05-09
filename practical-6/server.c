521060eba5170cadb95e6b064b1e2808a4cb0e5a
Tlholo Koma <tlholokoma@gmail.com>
2022-05-09 03:11:52 +0200

    :sparkles: starting new project

---
+++ practical-6/server.c
@@ -0,0 +1,73 @@
+/*
+* 20575085, Koma T(Tlholo)
+* 20456078, Dzimati BM(Malcolm)
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define PORT 5555
+#define MAX_LENGTH 256
+#define database ("database.txt")
+
+
+int main(int argc, char const *argv[])
+{
+    
+    
+    int server_fd, new_socket;
+    long valread;
+    struct sockaddr_in address;
+    int addrlen=sizeof(address);
+
+    //Creating socket
+    if ((server_fd=socket(AF_INET, SOCK_STREAM, 0))==0) {
+        perror("In socket");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family=AF_INET;
+    address.sin_addr.s_addr=INADDR_ANY;
+    address.sin_port=htons(PORT);
+
+    memset(address.sin_zero, '\0', sizeof(address.sin_zero));
+
+    // Connect to remote server
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
+        perror("In bind");
+        exit(EXIT_FAILURE);
+    }
+
+    // Adding listener
+    if (listen(server_fd, 10)<0) {
+        perror("In listen");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+        printf("Waiting for new connection...\n\n");
+
+        // Waiting for upcoming client
+        if ((new_socket=accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
+            perror("In accept");
+            exit(EXIT_FAILURE);
+        }
+
+
+        char buffer[1000]={0};
+        valread=read(new_socket, buffer, 1000);
+        write(new_socket, buffer, strlen(buffer));
+
+        close(new_socket);
+    }
+    
+    
+    return 0;
+}
