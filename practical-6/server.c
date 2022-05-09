f80165dcb1fe0af5e49f3f715ec8a41cc0676378
Tlholo Koma <tlholokoma@gmail.com>
2022-05-10 00:01:35 +0200

    :construction: adding commands according to rfc

--- practical-6/server.c
+++ practical-6/server.c
@@ -15,6 +15,7 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
+
 #define PORT 25
 #define MAX_LENGTH 256
 #define database ("database.txt")
@@ -64,6 +65,34 @@ int connectToServer(const char* server_add) {
         connect(socket_fd, (struct sockaddr*)&address, sizeof(address));
     }
     return socket_fd;
+}
+
+char* MailHeader(const char* from, const char* to, const char* subject, const char* mime_type, const char* charset){
+    time_t now;
+    time(&now);
+    char* mail_header = null;
+    char date_buff[26];
+    char Sender[6 + strlen(from) + 1 + 1];
+    char Recip[4 + strlen(to) + 1 + 1];
+    char Subject[8 + 1 + strlen(subject) + 1 + 1];
+    char mime_data[13 + 1 + 3 + 1 + 1 + 13 + 1 + strlen(mime_type) + 1 + 1 + 8 + strlen(charset) + 1 + 1 + 2];
+
+    strftime(date_buff, (33), "%a , %d %b %Y %H:%M:%S", localtime($now));
+
+    sprintf(Sender, "From: %s\r\n", from);
+    sprintf(Recip, "To: %s\r\n", to);
+    sprintf(Subject, "Subject: %s\r\n", subject);
+    sprintf(mime_date, "MIME-Version: 1.0\r\nContent-type: %s; charset=%s\r\n\r\n", mime_type, charset);
+
+    int mail_header_length = strlen(Subject) + strlen(Sender) + strlen(Recip) + strlen(mime_type) + 10;
+
+    mail_header = (char*) malloc(mail_heard_length * sizeof (char));
+
+    memcpy(&mail_header[0], &Sender, strlen(Sender));
+    memcpy(&mail_header[0 + strlen(Sender)], &Recip, strlen(Recip));
+    memcpy(&mail_header[0 + strlen(Sender) + strlen(Recip)], &Subject, strlen(Subject));
+    memcpy(&mail_header[0 + strlen(Sender) + strlen(Recip) + strlen(Subject)], &mime_data, strlen(mime_data));
+    return mail_header;
 }
 
 
