f08cae2346518bfe7b4420aaf38c01019ea095a2
malcolmdzimati <malcolmdzimati@gmail.com>
2022-05-22 23:32:11 +0200

    Added code to wtach the file within HTML folder, if the file is editted a string is printed use makefile to run code

--- practical-8/client.c
+++ practical-8/client.c
@@ -14,14 +14,58 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <sys/sendfile.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/inotify.h>
+#include <limits.h>
+#include <unistd.h>
 
 #define PORT 21
 #define STACK_MAX 100
+#define MAX_EVENTS 1024 /*Max. number of events to process at one go*/
+#define LEN_NAME 1024 /*Assuming length of the filename won't exceed 16 bytes*/
+#define EVENT_SIZE  ( sizeof (struct inotify_event) ) /*size of one event*/
+#define BUF_LEN     ( MAX_EVENTS * ( EVENT_SIZE + LEN_NAME )) /*buffer to store the data of events*/
 
+void get_event (int fd) {
+    char buffer[BUF_LEN];
+    int length, i = 0;
+
+    length = read( fd, buffer, BUF_LEN );
+    if ( length < 0 ) {
+        perror( "read" );
+    }
+
+    while ( i < length ) {
+        struct inotify_event *event = ( struct inotify_event * ) &buffer[ i ];
+        if ( event->len ) {
+            /*if ( event->mask & IN_CREATE) {
+                if (event->mask & IN_ISDIR)
+                    printf( "The directory %s was Created.\n", event->name );
+                else
+                    printf( "The file %s was Created with WD %d\n", event->name, event->wd );
+            }*/
+
+            if ( event->mask & IN_MODIFY) {
+                if (event->mask & IN_ISDIR)
+                    printf( "The directory %s was modified.\n", event->name );
+                else
+                    printf( "The file %s was modified with WD %d\n", event->name, event->wd );
+            }
+
+            /*if ( event->mask & IN_DELETE) {
+                if (event->mask & IN_ISDIR)
+                    printf( "The directory %s was deleted.\n", event->name );
+                else
+                    printf( "The file %s was deleted with WD %d\n", event->name, event->wd );
+            }*/
+            i += EVENT_SIZE + event->len;
+        }
+    }
+}
 
 int main(int argc, char const *argv[])
 {
-    
     int server_fd, new_socket;
     long valread;
     struct sockaddr_in address;
@@ -48,7 +92,7 @@ int main(int argc, char const *argv[])
     int i = 0;
     char buffer[256];
 
-    while (1) {
+    while (0) {
         printf("We're within");
 
         /*
@@ -66,6 +110,31 @@ int main(int argc, char const *argv[])
         */
         close(new_socket);
     }
+
+    int wd, fd;
+
+    fd = inotify_init();
+    if ( fd < 0 ) {
+        perror( "Couldn't initialize inotify");
+    }
+
+    wd = inotify_add_watch(fd, argv[1], IN_CREATE | IN_MODIFY | IN_DELETE);
+    if (wd == -1) {
+        printf("Couldn't add watch to %s\n",argv[1]);
+    } else {
+        printf("Watching:: %s\n",argv[1]);
+    }
+
+    /* do it forever*/
+    while(1) {
+        get_event(fd);
+    }
+
+    /* Clean up*/
+    inotify_rm_watch( fd, wd );
+    close( fd );
+
+    return 0;
     
     return 0;
 }
